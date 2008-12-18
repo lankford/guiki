91d9b014e6702b190b2f4fae8d8223e9b5c9992b
root <lankford@mattlankford.com>
2008-12-17 22:58:48 -0500

    forgotten dirs

--- index.php
+++ index.php
@@ -1,43 +1,18 @@
 <?php
 
+session_start();
+
 # Licence    : GPL 
 # Created by : Matt Lankford
 # Homepage   : Guiki.com
 
-session_start();
-
 # START of configuration section
-$_SESSION{'HOME'}	= "Guiki";
-$_SESSION{'USER'}	= "admin";
-$_SESSION{'PASS'}	= "password";
+$_SESSION{'HOME'} = "Guiki";
+$_SESSION{'USER'} = "admin";
+$_SESSION{'PASS'} = "password";
 # END of configuration section 
 
-# !!!!USE CAUTION BELOW HERE!!!!
-
-//TODO create a template system
-//TODO make the menu an include via {MENU} tag
-//TODO make the search for an include via {SEARCH} tag
-//TODO make the footer an include via the {FOOTER} tag
-//TODO make the header an include via the {HEADER} tag
-//TODO make the editor an include via the {EDITOR} tag
-//TODO make the sidebar an include via the {SIDEBAR} tag
-//TODO maybe I make all things includeable via a system so you can create your own tags
-//TODO make the login option on the menu a dropdown with logout, edit, delete and other admin functions
-//TODO maybe use sqlite as the backend?
-//TODO make a template dir and put templates in there and parse each one and parse them into session variables the same name as the file name
-//TODO make a Sqlite version (or store files as XML)
-//TODO add keywords area and description area
-# change the urls to do this site.com/template/content
-# so it could say site.com/show/Guiki
-# or site.com/edit/Guiki
-# or it could do this site.com/show/data/Guiki
-# and site.com/show/Documentation/About
-# or even do directorys like site.com/show/Documentation/About
-# I can also create modules like site.com/install
-# or site.com/backup
-# problem is URL rewrites must be used or site.com?m=module&p=page
-# or site.com?a=module/Documentation/About
-# then I could include an .htaccess and code for either situation
+###### USE CAUTION BELOW HERE ######
 
 setup();
 
@@ -52,17 +27,14 @@ function setup()
 	$_SESSION{'FLASH'}		= '';
 	$_SESSION{'OUTPUT'}		= '';
 	$_SESSION{'CONTENT'}	= '';
-	$_SESSION{'TEMPLATE'}	= 'show.html';
-	
-	# Set the mode to show if not already set
-	$_SESSION{'MODE'} = ($_GET{'MODE'}) ? clean($_GET{'MODE'}) : 'SHOW';
+	$_SESSION{'TEMPLATE'}	= 'templates/show.html';
 	
 	# Set the page to the default unless already set
 	$_SESSION{'PAGE'} = ($_GET{'PAGE'}) ? clean($_GET{'PAGE'}) : $_SESSION{'HOME'};
 }
 function build()
 {
-	switch($_SESSION{'MODE'})
+	switch(clean($_GET{'MODE'}))
 	{
 		case 'LOGOUT':
 			logout();
@@ -80,45 +52,32 @@ function build()
 			save();
 			break;
 		default:
-			defaults();
 			break;
 	}
 }
 function show()
 {
+	# Set the header to not cache files
 	header ("Expires: Mon, 26 Jul 1990 05:00:00 GMT");
 	header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 	header ("Cache-Control: no-cache, must-revalidate");
 	header ("Pragma: no-cache");
 
+	set_content();
+	
 	set_template();
 	
-	replace_variables();
+	write_wiki_links();
 	 
+	replace_variables();
+	
 	print $_SESSION{'OUTPUT'} ;
-}
-function defaults()
-{
-	if (set_content())
-	{
-		write_wiki_links();
-
-	} else {
-		
-		edit();
-	}
 }
 function edit()
 {
 	if(authenticate())
 	{
-		$_SESSION{'TEMPLATE'} = 'edit.html';
-
-		set_content();
-	
-	} else {
-		
-		$_SESSION{'ERROR'} .= "<p>You Must Be Logged In To Create Or Edit Files</p>";
+		$_SESSION{'TEMPLATE'} = 'templates/edit.html';
 	}
 }
 function save()
@@ -127,17 +86,12 @@ function save()
 	{
 		if (file_put_contents('data/'.$_SESSION{'PAGE'},stripcslashes($_POST{'CONTENT'})))
 		{
-			$_SESSION{'FLASH'} = 'File '.$_SESSION{'PAGE'}.' was saved';
-			
-			set_content();
-			
-			write_wiki_links();
-		}
-
-	} else {
+			$_SESSION{'FLASH'} = '<p>File '.$_SESSION{'PAGE'}.' was saved</p>';
 		
-		$_SESSION{'ERROR'} .= "You Must Login To Save Files";
-
+		} else {
+			
+			$_SESSION{'ERROR'} .= "<p>There Was An Error Saving The File : ".$_SESSION{'PAGE'}."</p>";
+		}
 	}
 }
 function delete()
@@ -149,23 +103,15 @@ function delete()
 			$_SESSION{'FLASH'} = '<p>File '.$_SESSION{'PAGE'}.' was deleted</p>';
 
 			$_SESSION{'PAGE'} = $_SESSION{'HOME'};
+		
+		} else {
 			
-			set_content();
-			
-			write_wiki_links();
+			$_SESSION{'ERROR'} .= "<p>There was an error deleting file : ".$_SESSION{'PAGE'}."</p>";
 		}
-	
-	} else {
-
-		$_SESSION{'ERROR'} .= "<p>You Must Login To Delete Files</p>";
 	}
 }
 function logout()
 {
-	set_content();
-	
-	write_wiki_links();
-	
 	$_SERVER['PHP_AUTH_USER'] = '';
 	
 	authenticate();
@@ -178,14 +124,15 @@ function authenticate()
 		{
 			return true;
 		}
-	}
+	} else {
 	
-	header('WWW-Authenticate: Basic realm="'.$_SESSION{'HOME'}.'"');
-	header('HTTP/1.0 401 Unauthorized');
+		header('WWW-Authenticate: Basic realm="'.$_SESSION{'HOME'}.'"');
+		header('HTTP/1.0 401 Unauthorized');
 
-	$_SESSION{'ERROR'} .= "<p>You Are NOT Logged In!</p>";
+		$_SESSION{'ERROR'} .= "<p>You Must Login To Create, Edit or Delete files!</p>";
 	
-	return false;
+		return false;
+	}
 }
 function clean($var)
 {
@@ -193,20 +140,31 @@ function clean($var)
 }
 function set_content()
 {
-	if (file_exists('data/'.$_SESSION{'PAGE'}))
+	# Skip if the user is searching
+	if ( ! clean($_POST{'SEARCH'}))
 	{
-		$_SESSION{'CONTENT'} = implode( "", file('data/'.$_SESSION{'PAGE'}));
+		if (file_exists('data/'.$_SESSION{'PAGE'}))
+		{
+			$_SESSION{'CONTENT'} = implode( "", file('data/'.$_SESSION{'PAGE'}));
 		
-		return true;
-	
-	} else {
+		} else {
 		
-		return false;
+			$_SESSION{'ERROR'} = "<p>This File Does not Exist!</p>";
+			
+			edit();
+		}
 	}
 }
 function set_template()
 {
-	$_SESSION{'OUTPUT'} = implode( "", file('templates/'.$_SESSION{'TEMPLATE'}));
+	if (file_exists($_SESSION{'TEMPLATE'}))
+	{
+		$_SESSION{'OUTPUT'} = implode( "", file($_SESSION{'TEMPLATE'}));
+	
+	} else {
+	
+		print "<p></p>Could not open template : ".$_SESSION{'TEMPLATE'}."</p>";
+	}
 }
 function replace_variables()
 {
@@ -220,7 +178,10 @@ function replace_variables()
 }
 function write_wiki_links()
 {
-	$_SESSION{'CONTENT'} = preg_replace("/\[\[(.*?)\]\]/",writeLink("\\1"),$_SESSION{'CONTENT'});
+	if ($_SESSION{'TEMPLATE'} != 'templates/edit.html')
+	{
+		$_SESSION{'CONTENT'} = preg_replace("/\[\[(.*?)\]\]/",writeLink("\\1"),$_SESSION{'CONTENT'});
+	}
 }
 function writeLink($PAGE)
 {
